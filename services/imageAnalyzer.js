b9efb5a4561de28117e2926db0e620f3515698c3
MEGratron <arnaldinalimadias@gmail.com>
2026-02-21 15:57:23 +0000

    Update imageAnalyzer.js

--- services/imageAnalyzer.js
+++ services/imageAnalyzer.js
@@ -1,1 +1,7 @@
+import Tesseract from "tesseract.js";
+import { analyzeText } from "./textAnalyzer.js";
 
+export async function analyzeImage(path) {
+  const { data: { text } } = await Tesseract.recognize(path, "eng");
+  return analyzeText(text);
+}
